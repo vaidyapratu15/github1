30117851244999e91eef8645dd89d2b9abd81ba6
lenovo <lenovo@DESKTOP-A7R9JG9>
2022-07-17 17:51:39 +0530

    [pratiksha] new demo msg added

---
+++ src/Git/Ex_1_github.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class Ex_1_github 
+{
+        public static void main(String[] args) 
+        {
+        	
+        	System.out.println("welcome");
+        	System.out.println("Prtz");
+			
+		}
+}
